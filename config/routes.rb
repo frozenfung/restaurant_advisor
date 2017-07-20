ce38c8aca85f54f8f4d45278534f9d9c55781e29
jimmy <jimmyyang5296@gmail.com>
2017-07-20 19:21:35 +0800

    add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root :to => "restaurants#index"
+  root :to => "restaurants#index"
+
+  namespace :admin do
+    resources :restaurants
+  end
+
 end
