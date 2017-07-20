0c218682efa9c5d2319d44ee935118c4e2cdad8d
jimmy <jimmyyang5296@gmail.com>
2017-07-20 19:54:34 +0800

    admin`s index / new /create

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,4 +1,29 @@
 class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!  # 這個是 devise 提供的方法，先檢查必須登入
   before_action :authenticate_admin # 再檢查是否有admin權限
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to :action => :index
+  end
+
+  protected
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :opening_hour, :tel, :address, :description, :image)
+  end
+
+
+
+
+
 end
