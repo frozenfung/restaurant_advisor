3e614191dda9a4c0d085c2fe4c89c430e2cbfa8a
jimmy <jimmyyang5296@gmail.com>
2017-07-21 14:54:07 +0800

    seeds 種子資料建立（餐廳的分類）

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category_list =[
+	{"name" => "中式餐廳"},
+	{"name" => "日本餐廳"},
+  {"name" => "義大利餐廳"},
+  {"name" => "墨西哥餐廳"},
+  {"name" => "素食餐廳"},
+  {"name" => "美式餐廳"},
+  {"name" => "複合式餐廳"}
+
+]
+category_list.each do |category|
+  Category.create( :name => category["name"] )
+end
